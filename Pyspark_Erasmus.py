22a05f83c63243b9e2fb619d6a64de2fa5f7d2a7
Dan <dan.gheorghita@student.upt.ro>
2023-07-29 15:58:44 +0300

    Create a table for each selected country in a mysql database

--- Pyspark_Erasmus.py
+++ Pyspark_Erasmus.py
@@ -5,6 +5,7 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder \
       .master("local[1]") \
       .appName("SparkByExamples.com") \
+      .config("spark.jars", "mysql-connector-j-8.1.0.jar") \
       .getOrCreate()
 
 # Read from csv, create a DataFrame
@@ -13,5 +14,39 @@ df = spark.read.csv("Erasmus.csv", header=True)
 
 # Shows every country how many students it received from the others country
 filtered_df = df.groupby("Receiving Country Code", "Sending Country Code").count().sort("Receiving Country Code", "Sending Country Code")
+
+# Filtrare dupa coduri tari (daca sunt LV MK MT)
+three_countries = ['LV', 'MK', 'MT']
+three_df = filtered_df.filter(df['Receiving Country Code'].isin(three_countries))
+
 # Print table
 filtered_df.show(400)
+
+# # Write to MySQL Table
+three_df.write \
+  .format("jdbc") \
+  .option("driver", "com.mysql.cj.jdbc.Driver") \
+  .option("url", "jdbc:mysql://localhost:3306/erasmus") \
+  .option("dbtable", "Three_Countries") \
+  .option("user", "root") \
+  .option("password", "") \
+  .mode("overwrite") \
+  .save()
+
+# store data regarding mobilities from a list of given receiving codes
+def tabels_countries():
+    for country in three_countries:
+        table_name = country + "_Receiving"
+        country_df = three_df .filter(three_df["Receiving Country Code"] == country).drop("Receiving Country Code")
+        country_df.write \
+            .format("jdbc") \
+            .option("driver", "com.mysql.cj.jdbc.Driver") \
+            .option("url", "jdbc:mysql://localhost:3306/erasmus") \
+            .option("dbtable", table_name) \
+            .option("user", "root") \
+            .option("password", "") \
+            .mode("overwrite") \
+            .save()
+
+
+tabels_countries()
